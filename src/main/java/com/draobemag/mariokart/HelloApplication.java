afb2d52dc83e6dd38bcf2e1f6afa0a90937fb938
Loiosh <30377390+Spellbreeze@users.noreply.github.com>
2021-09-27 13:10:15 -0400

    example of scene singleton use

--- src/main/java/com/draobemag/mariokart/HelloApplication.java
+++ src/main/java/com/draobemag/mariokart/HelloApplication.java
@@ -1,5 +1,6 @@
 package com.draobemag.mariokart;
 
+import com.draobemag.mariokart.Singletons.SceneManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,10 +11,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 420, 240);
         stage.setTitle("Hello!");
-        stage.setScene(scene);
+        stage.setScene(SceneManager.LoadScene());
         stage.show();
     }
 
--- src/main/java/com/draobemag/mariokart/Singletons/SceneManager.java
+++ src/main/java/com/draobemag/mariokart/Singletons/SceneManager.java
@@ -1,5 +1,11 @@
 package com.draobemag.mariokart.Singletons;
 
+import com.draobemag.mariokart.HelloApplication;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+
+import java.io.IOException;
+
 public class SceneManager {
     private static SceneManager instance = null;
 
@@ -17,5 +23,12 @@ public class SceneManager {
             instance = new SceneManager();
         }
         return instance;
+    }
+
+    public static Scene LoadScene() throws IOException {
+        //Currently only loads welcome scene
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("welcome-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 420, 240);
+        return scene;
     }
 }
