f489088360cce2a69285f8b28f3c92d5903e823e
Jakob Johnson <johnsonjakob99@gmail.com>
2021-10-02 11:31:49 -0400

    Add simple TestFX that test the first two screens and update onPlayerConfigClick to appropriately handle no selection

---
+++ src/test/java/GameScreensTest.java
@@ -0,0 +1,52 @@
+import com.draobemag.mariokart.HelloApplication;
+
+
+import org.junit.Test;
+
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.scene.text.Text;
+
+import org.testfx.framework.junit.ApplicationTest;
+import org.testfx.matcher.base.NodeMatchers;
+
+import static org.junit.Assert.assertNotNull;
+import static org.testfx.api.FxAssert.verifyThat;
+
+
+
+
+public class GameScreensTest extends ApplicationTest {
+    private  Stage myStage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        HelloApplication helloApplication = new HelloApplication();
+        helloApplication.start(primaryStage);
+        myStage = primaryStage;
+    }
+
+    @Test
+    public void testWelcomeScreenLaunched() {
+        // Ensure the 'Start Your Engines!' button is present
+        verifyThat("Start Your Engines!", NodeMatchers.isVisible());
+    }
+
+    @Test
+    public void testNextScreen() {
+        clickOn("#initStart");
+        // Check that we progressed to the next screen by ensuring the 'Go!' button is present
+        verifyThat("Go!", NodeMatchers.isVisible());
+    }
+
+    @Test
+    public void testNumDriversAlert() {
+        // Advance to next screen
+        clickOn("#initStart");
+        // Alert should occur since the number of drivers was not selected
+        clickOn("#playerConfig");
+        // Get the Node of the Alert
+        Node alertNode = lookup(".dialog-pane").query();
+        assertNotNull(alertNode);
+    }
+}
